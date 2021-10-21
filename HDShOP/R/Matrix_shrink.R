d34c49e8ac4d1a4a2299ac10ddb1733ccbf3bcc6
nparolya <72650247+nparolya@users.noreply.github.com>
2021-10-21 10:47:00 +0200

    Update Matrix_shrink.R

--- HDShOP/R/Matrix_shrink.R
+++ HDShOP/R/Matrix_shrink.R
@@ -73,6 +73,8 @@ nonlin_shrinkLW = function(x){
   eig = eigen(sampleC)
   u = eig$vectors[,p:1]
   lambda = rev(eig$values)
+
+  if(p<=n){
   lambda = lambda[max(1, p-n+1):p]
   L = matrix(rep(lambda, min(p, n)), nrow = length(lambda))
   h = n^(-1/3)
@@ -82,14 +84,22 @@ nonlin_shrinkLW = function(x){
   Hftemp = (-3/10/pi) * x + (3/4/sqrt(5)/pi) * (1 - x^2./5) * log(abs((sqrt(5) - x)/(sqrt(5) + x)))
   Hftemp[abs(x) == sqrt(5)] = (-3/10/pi) * x[abs(x) == sqrt(5)]
   Hftilde = rowMeans(Hftemp / H)
-
-  if(p<=n){
     dtilde = lambda / ((pi*(p/n)*lambda*ftilde)^2 + (1-(p/n)-pi*(p/n)*lambda*Hftilde)^2);
   }else{
+lambda = lambda[max(1, p-n+2):p]
+  L = matrix(rep(lambda, min(p, n-1)), nrow = length(lambda))
+  h = n^(-1/3)
+  H = h * t(L)
+  x <- (L - t(L)) / H # This is a different x than before
+  ftilde = (3/4/sqrt(5)) * rowMeans(pmax(1-x^2/5, 0) / H)
+  Hftemp = (-3/10/pi) * x + (3/4/sqrt(5)/pi) * (1 - x^2./5) * log(abs((sqrt(5) - x)/(sqrt(5) + x)))
+  Hftemp[abs(x) == sqrt(5)] = (-3/10/pi) * x[abs(x) == sqrt(5)]
+  Hftilde = rowMeans(Hftemp / H)
+
     Hftilde0 = (1/pi)*(3/10/h^2+3/4/sqrt(5)/h*(1-1/5/h^2) *log((1+sqrt(5)*h)/(1-sqrt(5)*h))) * mean(1/lambda)
     dtilde0=1/(pi*(p-n)/n*Hftilde0)
     dtilde1 = lambda/(pi^2*lambda^2 * (ftilde^2+Hftilde^2))
-    dtilde = c(dtilde0 * rep(1, p-n), dtilde1)
+    dtilde = c(dtilde0 * rep(1, p-n+1), dtilde1)
   }
   u %*% diag(dtilde) %*% t(u)
 } # analytical nonlinear shrinkage
