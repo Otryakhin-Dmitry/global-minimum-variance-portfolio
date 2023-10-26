52d829e1627a5d4ee25c965c2f8e9a68830c44db
Otryakhin-Dmitry <8742585+Otryakhin-Dmitry@users.noreply.github.com>
2023-10-26 23:22:43 +0200

    Improved readability in data.R

--- HDShOP/R/data.R
+++ HDShOP/R/data.R
@@ -1,8 +1,10 @@
 #' Daily  log-returns  of selected constituents S&P500.
 #'
-#' Daily log-returns of selected constituents of S&P500 in percents. The data are sampled
-#' in business time, i.e., weekends and holidays are omitted.
+#' Daily log-returns of selected constituents of S&P500 in percents. 
+#' The data are sampled in business time, i.e., weekends and holidays 
+#' are omitted.
 #'
-#' @format a matrix with the first column containing the data and company names as column labels.
+#' @format a matrix with the first column containing the data and company 
+#' names as column labels.
 #' @source Yahoo finance
 "SP_daily_asset_returns"
